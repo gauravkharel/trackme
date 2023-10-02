81f3284cad4f347bfd42843d5868e2a2241574c3
gauravkharel <kharel619@gmail.com>
2023-10-02 10:31:55 +0545

    feat: Adding callback to add user in DB

---
+++ prisma/migrations/20231002042546_avatar/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "Activity" ALTER COLUMN "id" SET DEFAULT concat('act_', replace(cast(gen_random_uuid() as text), '-', ''));
+
+-- AlterTable
+ALTER TABLE "Tenant" ALTER COLUMN "id" SET DEFAULT concat('tnt_', replace(cast(gen_random_uuid() as text), '-', '')),
+ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT,
+ALTER COLUMN "id" SET DEFAULT concat('usr_', replace(cast(gen_random_uuid() as text), '-', ''));
